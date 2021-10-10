5ccae10ae6cbfcf6fdf288dbc6d507c385eb187f
udith-shalinda <udithshalinda2@gmail.com>
2021-10-10 16:51:17 +0530

    api updated with serverless url

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,2 +1,3 @@
-export const API_URL = "http://10.0.2.2:3000/dev"
+// export const API_URL = "http://10.0.2.2:3000/dev"
+export const API_URL = "https://3zm0x6i9s9.execute-api.us-east-1.amazonaws.com/dev"
 export const googleId = "321671343547-n776ci66rkkdtjn1kg76a9bsh2d6fkco.apps.googleusercontent.com";
